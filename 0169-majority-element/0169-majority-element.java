1baf0da268424e6ec9970690d799ac996748314b
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-08-05 22:08:59 +0530

    Time: 1 ms (99.79%), Space: 53.4 MB (12.02%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority=nums[0];
+        int vote=1;
+        for(int i=1;i<nums.length;i++){
+            if(majority==nums[i]){
+                vote++;
+            }
+            else if(vote==0){
+                majority=nums[i];
+                vote++;
+            }
+            else{
+                vote--;
+            }
+        }
+        return majority;
+        
+    }
+}
