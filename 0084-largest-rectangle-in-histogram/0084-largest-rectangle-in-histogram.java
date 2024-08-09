baf9d98c18a24150faf5d2b6875f64c698c3ee2a
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-08-09 22:51:59 +0530

    Time: 115 ms (19.10%), Space: 57.9 MB (23.40%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,51 +1,56 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-     int n =heights.length;
-     Stack<Integer> st=new Stack<>();
-     int[] nse=new int[n];
-     int[] pse=new int[n];
-     // calculating nse( next smalller element)
-     st.push(n-1);
-     nse[n-1]=n;
-     for(int i=n-2;i>=0;i--){
-        while(st.size()>0 && heights[i]<=heights[st.peek()]){
-            st.pop();
-        }
-        if(st.size()==0){
+    
+    public int[] find_nse(int[] heights){
+        int n=heights.length;
+        Stack<Integer> st=new Stack<>();
+        int[] nse=new int[n];
+        
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0 && heights[st.peek()]>=heights[i]){
+                st.pop();
+            }
+            if(st.size()==0){
                 nse[i]=n;
             }
-        else{
+            else{
                 nse[i]=st.peek();
             }
-        st.push(i);
-     }
-     //-----> emptying the stack
-     
-     while(st.size()>0){
-        st.pop();
-     }
-     // for pse (previous smaller element)
-       st.push(0);
-       pse[0]=-1;
-       for(int i=1;i<n;i++){
-       while(st.size()>0 && heights[i]<=heights[st.peek()]){
-        st.pop();
-       }
-       if(st.size()==0){
-        pse[i]=-1;
-       }
-       else{
-        pse[i]=st.peek();
-       }
-       st.push(i);
-     }
-     //---> maximum area of rectangle
-     int max =-1;
-     for(int i=0;i<n;i++){
-        int area=heights[i]*(nse[i]-pse[i]-1);
-        max=Math.max(max,area);
-     }
-     return max;
-   }
-
+            st.push(i);
+        }
+        return nse;
+    }
+    
+    public int[] find_pse(int[] heights){
+        int n=heights.length;
+        Stack<Integer> st=new Stack<>();
+        int[] pse=new int[n];
+       
+        for(int i=0;i<n;i++){
+            while(st.size()>0 && heights[st.peek()]>=heights[i]){
+                st.pop();
+            }
+            if(st.size()==0){
+                pse[i]=-1;
+            }
+            else{
+                pse[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return pse;
+    }
+    
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        int[] nse=find_nse( heights);
+        int[] pse=find_pse( heights);
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            int h=heights[i];
+            int w=nse[i]-pse[i]-1;
+            int area=h*w;
+            max=Math.max(max,area);
+        }
+        return max;
+    }
 }
