3ebcdeb78662e6f1a4b6280bb29ba5413e6ec47b
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-08-13 22:39:20 +0530

    Time: 1 ms (13.70%), Space: 41.8 MB (26.39%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,62 +13,26 @@
  *     }
  * }
  */
-// class Solution {
-//     public boolean isSymmetric(TreeNode root) {
-//         if(root==null) return true;
-//         Queue<TreeNode> leftTree=new LinkedList<>();
-//         Queue<TreeNode> rightTree=new LinkedList<>();
-//         leftTree.add(root.left);
-//         rightTree.add(root.right);
-//         while(!leftTree.isEmpty()  && !rightTree.isEmpty()){
-//             TreeNode leftNode=leftTree.poll();
-//             TreeNode rightNode=rightTree.poll();
-//             if(leftNode==null && rightNode==null) continue;
-//             if(leftNode==null || rightNode==null) return false;
-//             if(leftNode.val!=rightNode.val) return false;
-            
-//             leftTree.add(leftNode.left);
-//             leftTree.add(leftNode.right);
-//             leftTree.add(rightNode.right);
-//             leftTree.add(rightNode.left);
-//         }
-//         return true;
-//     }
-// }
-
 class Solution {
-
-  // iterative method to determine if a binary tree is symmetric
-  // using 2 queues
-  boolean isSymmetric(TreeNode root) {
-
-      if (root == null) return true;
-
-      Queue<TreeNode> leftTree = new LinkedList<>();
-      Queue<TreeNode> rightTree = new LinkedList<>();
-
-      leftTree.add(root.left);
-      rightTree.add(root.right);
-
-      while (!leftTree.isEmpty() && !rightTree.isEmpty()) {
-
-        TreeNode leftNode = leftTree.poll();
-        TreeNode rightNode = rightTree.poll();
-
-        if (leftNode == null && rightNode == null) continue;
-
-        if (leftNode == null || rightNode == null) return false;
-
-        if (leftNode.val != rightNode.val) return false;
-
-        // Pushing order is very important
-        leftTree.add(leftNode.left);
-        leftTree.add(leftNode.right);
-        rightTree.add(rightNode.right);
-        rightTree.add(rightNode.left);
-      }
-
-      return true;
-  }
-
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        Queue<TreeNode> leftTree=new LinkedList<>();
+        Queue<TreeNode> rightTree=new LinkedList<>();
+        leftTree.add(root.left);
+        rightTree.add(root.right);
+        while(!leftTree.isEmpty()  && !rightTree.isEmpty()){
+            TreeNode leftNode=leftTree.poll();
+            TreeNode rightNode=rightTree.poll();
+            if(leftNode==null && rightNode==null) continue;
+            if(leftNode==null || rightNode==null) return false;
+            if(leftNode.val!=rightNode.val) return false;
+            
+            leftTree.add(leftNode.left);
+            leftTree.add(leftNode.right);
+            rightTree.add(rightNode.right);
+            rightTree.add(rightNode.left);
+        }
+        return true;
+    }
 }
+
