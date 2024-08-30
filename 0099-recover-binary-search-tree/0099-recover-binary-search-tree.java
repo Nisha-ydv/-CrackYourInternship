c11007ed7794c8c5c9fb64fef80c43b909068558
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-08-30 22:14:54 +0530

    Time: 1 ms (100.00%), Space: 44.4 MB (59.07%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -13,68 +13,68 @@
  *     }
  * }
  */
-// class Solution {
-//     private TreeNode first;
-//     private TreeNode prev;
-//     private TreeNode middle;
+ class Solution {
+     private TreeNode first;
+     private TreeNode prev;
+     private TreeNode middle;
+       
+     private void inorder(TreeNode root){
+         if(root==null) return;
+         inorder(root.left);
+         if(  prev!=null && root.val<prev.val){
+             if(first==null) {
+                 first=prev;
+                
+             }
+            middle=root;
+             
+         }
+         prev=root;
+             inorder(root.right);
+     }
+     public void recoverTree(TreeNode root) {
+        first=middle=null;
+         prev=new TreeNode(Integer.MIN_VALUE);
+         inorder(root);
+         if( first!=null && middle!=null){
+             int temp=first.val;
+             first.val=middle.val;
+             middle.val=temp;
+         }
+             
+        
+    }
+ }
 
-//     private void inorder(TreeNode root){
-//         if(root==null) return;
+// class Solution {
+//     TreeNode prev;
+//     TreeNode violation1;
+//     TreeNode violation2;
+	
+//     public void inorder(TreeNode root) {
+//         if(root == null)
+//             return;
+        
 //         inorder(root.left);
-//         if(root.val<prev.val){
-//             if(first==null) {
-//                 first=prev;
-//                 middle=root;
-//             }
-//             else{
-//                 middle=root;
-//             }
-//             prev=root;
-//             inorder(root.right);
-//         }
-//     }
-//     public void recoverTree(TreeNode root) {
-//        first=middle=null;
-//         prev=new TreeNode(Integer.MIN_VALUE);
-//         inorder(root);
-//         if(first!=null && middle!=null){
-//             int temp=first.val;
-//             first.val=middle.val;
-//             middle.val=temp;
+        
+//         if(prev != null && prev.val > root.val)
+//  {
+
+//                 if(violation1 == null)
+//                     violation1 = prev;
+//                 violation2 = root;
 //         }
         
+//         prev = root;
+//         inorder(root.right);
+//     }
+
+//     public void recoverTree(TreeNode root) {
+//         inorder(root);
+        
+//         int temp = violation1.val;
+        
+//         violation1.val = violation2.val;
+//         violation2.val = temp;
 //     }
 // }
-
-class Solution {
-    TreeNode prev;
-    TreeNode violation1;
-    TreeNode violation2;
-	
-    public void inorder(TreeNode root) {
-        if(root == null)
-            return;
-        
-        inorder(root.left);
-        
-        if(prev != null && prev.val > root.val)
- {
-
-                if(violation1 == null)
-                    violation1 = prev;
-                violation2 = root;
-        }
-        
-        prev = root;
-        inorder(root.right);
-    }
-
-    public void recoverTree(TreeNode root) {
-        inorder(root);
-        
-        int temp = violation1.val;
-        
-        violation1.val = violation2.val;
-        violation2.val = temp;
-    }
-}
