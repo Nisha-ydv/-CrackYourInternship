911ff68a614983b889666abc83d1ed34a91ed5a1
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-09-01 21:12:18 +0530

    Time: 1 ms (37.54%), Space: 44.8 MB (55.79%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -24,27 +24,48 @@
  * }
  */
 class Solution {
-    private ListNode middleNode(ListNode head, ListNode tail){
-        ListNode slow=head;
-        ListNode fast=head;
-        ListNode prev=null;
-        while(fast!=null && fast.next!=null){
-            prev=slow;
-            slow=slow.next;
-            fast=fast.next.next;
+
+    public TreeNode sortedListToBST(ListNode head){
+        List<Integer> list=new ArrayList<>();
+        ListNode temp=head;
+        while(temp!=null){
+            list.add(temp.val);
+            temp=temp.next;
         }
-        if(prev!=null)
-            prev.next=null;
+        return dfs(list,0,list.size()-1);
+            
         
-        return slow;
     }
-    public TreeNode sortedListToBST(ListNode head) {
-      if(head==null) return null;
-        ListNode middle=middleNode(head,null);
-        TreeNode root=new TreeNode(middle.val);
-        if(middle==head) return root;
-        root.left=sortedListToBST(head);
-        root.right=sortedListToBST(middle.next);
+    private TreeNode dfs(List<Integer> list,int l, int r){
+        if(l>r) return null;
+        int mid=l+(r-l)/2;
+        TreeNode root=new TreeNode(list.get(mid));
+        root.left=dfs(list,l,mid-1);
+        root.right=dfs(list,mid+1,r);
         return root;
     }
+    
 }
+//private ListNode middleNode(ListNode head, ListNode tail){
+//         ListNode slow=head;
+//         ListNode fast=head;
+//         ListNode prev=null;
+//         while(fast!=null && fast.next!=null){
+//             prev=slow;
+//             slow=slow.next;
+//             fast=fast.next.next;
+//         }
+//         if(prev!=null)
+//             prev.next=null;
+        
+//         return slow;
+//     }
+//     public TreeNode sortedListToBST(ListNode head) {
+//       if(head==null) return null;
+//         ListNode middle=middleNode(head,null);
+//         TreeNode root=new TreeNode(middle.val);
+//         if(middle==head) return root;
+//         root.left=sortedListToBST(head);
+//         root.right=sortedListToBST(middle.next);
+//         return root;
+//     }
