b3a67b70574e541d2e4f9a946f91437c44648bd6
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-09-28 13:01:26 +0530

    Time: 13 ms (83.55%), Space: 47 MB (73.64%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,43 +1,38 @@
 class Solution {
- 
-    int[][] directions = {{-1, 0,1,0}, {0,1,0,-1}};
-    
-    public int[][] updateMatrix(int[][] matrix) {
-      int m = matrix.length;
-       int n = matrix[0].length;
+    public int[][] updateMatrix(int[][] mat) {
+       int m=mat.length;
+        int n=mat[0].length;
+        int[][] result=new int[m][n];
         
-        int[][] result = new int[m][n];
+        Queue<int[]> q=new LinkedList<>();
+        int[][] direction={{-1,0,1,0},{0,1,0,-1}};
         
-        Queue<int[]> que = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    result[i][j] = 0;
-                    que.offer(new int[] {i, j});
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    result[i][j]=0;
+                    q.add(new int[] {i,j});
                 }
-                else {
-                    result[i][j] = -1;
-                }
-            }
-        }
-
-        while (!que.isEmpty()) {
-            int[] cell = que.poll();
-            int i = cell[0];
-            int j = cell[1];
-            
-            for (int k=0;k<4;k++) {
-                
-                int new_i = i + directions[0][k];
-                int new_j = j + directions[1][k];
-                
-                if(new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && result[new_i][new_j] == -1) {
-                    result[new_i][new_j] = result[i][j] + 1;
-                    que.add(new int[] {new_i, new_j});
+                else{
+                    result[i][j]=-1;
                 }
             }
         }
         
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
+            int row=curr[0];
+            int col=curr[1];
+            for(int i=0;i<4;i++){
+                int new_row=row+direction[0][i];
+                int new_col=col+direction[1][i];
+                
+                if(new_row>=0 && new_row<m && new_col>=0 && new_col<n && result[new_row][new_col]==-1){
+                    result[new_row][new_col]=result[row][col]+1;
+                    q.add(new int[] {new_row,new_col});
+                }
+            }
+        }
         return result;
     }
 }
