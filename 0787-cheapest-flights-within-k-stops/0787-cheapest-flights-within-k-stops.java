2b598fb9507822e0a8b5cef8bcea5852451a3668
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-10-01 12:50:25 +0530

    Time: 4 ms (98.58%), Space: 44.2 MB (59.99%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,22 +1,24 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int [] prev = new int[n];
-        Arrays.fill(prev,Integer.MAX_VALUE);
-        prev[src]=0;
+        int [] dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
         for(int i=0;i<=k;i++){
             int cur[]= new int[n];
             for(int j=0;j<n;j++){
-                cur[j]= prev[j];
+                cur[j]= dist[j];       // copy of dist array
             }
-            for(int e[]:flights){
-                int u=e[0],v=e[1],wt=e[2];
-                if(prev[u] != Integer.MAX_VALUE && prev[u]+wt <cur[v]){
-                    cur[v] = prev[u]+wt;
+            for(int flight[]:flights){
+                int u=flight[0];
+                  int  v=flight[1];
+                 int   wt=flight[2];
+                if(dist[u] != Integer.MAX_VALUE && dist[u]+wt <cur[v]){
+                    cur[v] = dist[u]+wt;
                 }
             }
             for(int j=0;j<n;j++){
-                prev[j]=cur[j];
+                dist[j]=cur[j];    // put all values of curr into dist
             }
-        }return prev[dst]==Integer.MAX_VALUE ?-1:prev[dst];
+        }return dist[dst]==Integer.MAX_VALUE ?-1:dist[dst];
     }
 }
