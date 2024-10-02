9ae79725af620531bf889f714459382c35e5dbe0
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-10-02 15:24:58 +0530

    Time: 40 ms (25.62%), Space: 63.1 MB (13.55%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -17,56 +17,62 @@
     
     
     
-    
+//     class Solution {
 //     public int[] arrayRankTransform(int[] arr) {
-//         int n=arr.length;
-//         PriorityQueue<int[]> pq=new PriorityQueue<>(new Comparator<int[]>(){
-//             public int compare(int x[],int[] y){
-//                 return x[0]-y[0];
+//         int n = arr.length;
+//         //(element, index)
+//         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+//             public int compare(int x[], int y[]){
+//                 return x[0] - y[0]; //cust comp, inc sort
 //             }
 //         });
-        
 //         for(int i=0;i<n;i++){
-//           pq.add(new int[]{arr[i],i});  
+//             pq.offer(new int[]{arr[i],i});
 //         }
-        
+       
 //         int rank=0;
+//         int prev=Integer.MAX_VALUE;
 //         while(!pq.isEmpty()){
-//             int prev=Integer.MAX_VALUE;
-//             int[] curr= pq.poll();
-//             if(prev!=curr[0]){
+//             int cur[] = pq.poll();
+//             if(prev!=cur[0]){
+//                 prev = cur[0];
 //                 rank++;
 //             }
-//             arr[curr[1]]=rank;
+//             arr[cur[1]] = rank;
 //         }
 //         return arr;
 //     }
 // }
-    
-    
-    class Solution {
-    public int[] arrayRankTransform(int[] arr) {
-        int n = arr.length;
-        //(element, index)
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int x[], int y[]){
-                return x[0] - y[0]; //cust comp, inc sort
+
+
+
+
+
+class Solution{
+    public int[] arrayRankTransform(int[] arr){
+        ArrayList<Integer> index=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            index.add(i);
+        }
+        
+        Collections.sort(index, new Comparator<Integer>(){
+            public int compare(Integer x, Integer y){
+                return arr[x] - arr[y]; //cust comp, inc sort
             }
         });
-        for(int i=0;i<n;i++){
-            pq.offer(new int[]{arr[i],i});
-        }
-       
         int rank=0;
-        int prev=Integer.MAX_VALUE;
-        while(!pq.isEmpty()){
-            int cur[] = pq.poll();
-            if(prev!=cur[0]){
-                prev = cur[0];
+        int[] res=new int[arr.length];
+      int  prev=Integer.MAX_VALUE;
+        for(int ind:index){
+            if(prev!=arr[ind]){
+                prev=arr[ind];
                 rank++;
             }
-            arr[cur[1]] = rank;
+            res[ind]=rank;
         }
-        return arr;
+        return res;
     }
 }
+
+
+
