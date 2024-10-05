c57ee79284f5083aecbfa475b5a8e3ad29186440
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-10-05 17:44:51 +0530

    Time: 7 ms (72.48%), Space: 41.9 MB (99.68%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,3 +1,34 @@
+// class Solution {
+//     public boolean isMatched(int[] map1, int[] map2){
+//         for(int i=0;i<26;i++){
+//             if(map1[i]!=map2[i]) return false;
+//         }
+//         return true;
+//     }
+//     public boolean checkInclusion(String s1, String s2) {
+//       int n=s1.length();
+//         int m=s2.length();
+//         int[] map1=new int[26];
+        
+//           if(m<n) return false;
+        
+//         for(int i=0;i<n;i++){
+//             map1[s1.charAt(i)-'a']++;
+//         }
+        
+//         for(int i=0;i<=m-n;i++){
+//             int[] map2=new int[26];
+//             for(int j=0;j<n;j++){
+//                map2[s2.charAt(i+j)-'a']++; 
+//             }
+//             if(isMatched(map1,map2)) return true;
+//         }
+//         return false;
+//     }
+// }
+
+
+
 class Solution {
     public boolean isMatched(int[] map1, int[] map2){
         for(int i=0;i<26;i++){
@@ -9,19 +40,22 @@ class Solution {
       int n=s1.length();
         int m=s2.length();
         int[] map1=new int[26];
+        int[] map2=new int[26];
         
           if(m<n) return false;
         
         for(int i=0;i<n;i++){
             map1[s1.charAt(i)-'a']++;
+            map2[s2.charAt(i)-'a']++; 
         }
+        if(isMatched(map1,map2)) return true;
         
-        for(int i=0;i<=m-n;i++){
-            int[] map2=new int[26];
-            for(int j=0;j<n;j++){
-               map2[s2.charAt(i+j)-'a']++; 
-            }
+        for(int i=1;i<=m-n;i++){
+        map2[s2.charAt(i-1)-'a']--;   //deletion
+            map2[s2.charAt(i+n-1)-'a']++;     //addition
             if(isMatched(map1,map2)) return true;
+            
+            
         }
         return false;
     }
