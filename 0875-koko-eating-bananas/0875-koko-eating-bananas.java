e07f749b5b684a0cb817de3a5f7ec71cdc7ed2d8
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-10-08 20:03:53 +0530

    Time: 12 ms (81.19%), Space: 45.2 MB (26.86%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,74 @@
+// class Solution {
+//     public boolean isEatingSpeed(int[] piles,int eatingspeed,int h){
+//         int hourspend=0;
+//         for(int i=0;i<piles.length;i++){
+//             hourspend+=piles[i]/eatingspeed;
+//             if(piles[i]%eatingspeed!=0){
+//                 hourspend+=1;
+//             }
+//             if(hourspend>h) return false;
+//         }
+        
+//         return true;
+        
+//     }
+//     public int minEatingSpeed(int[] piles, int h) {
+//      int start=1;
+//         int end=Integer.MIN_VALUE;
+//         for(int i=0;i<piles.length;i++){
+//             if(piles[i]>end){
+//                 end=piles[i];
+//             }
+//         }
+//         int ans=-1;
+//         while(start<=end){
+//           int mid=start+(end-start)/2;
+//            if(isEatingSpeed(piles,mid,h)){
+//             ans=mid;
+//             end=mid-1;
+//             }
+//              else{
+//             start=mid+1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int start = 1;
+        int end = Integer.MIN_VALUE; // for finding largest value in piles
+
+        // finding max
+        for(int i=0;i<piles.length;i++){
+            if(piles[i]>end){
+                end = piles[i];
+            }
+        }
+        int ans = -1;
+        // apply binary search on the possible values of K -> (1, max(piles))
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(isEatingSpeedValid(piles, mid, h)){
+                ans = mid;
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    boolean isEatingSpeedValid(int piles[], int eatingSpeed, int hours) {
+        int hoursSpent = 0;
+        for(int i=0;i<piles.length;i++){
+            hoursSpent += piles[i]/eatingSpeed;
+            if(piles[i]%eatingSpeed!=0){
+                hoursSpent += 1;
+            }
+            if(hoursSpent>hours) return false;
+        }
+        return true;
+    }
+}
