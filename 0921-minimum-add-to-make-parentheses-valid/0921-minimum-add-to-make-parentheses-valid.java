a2bf76688b6383fd34bf7f008db3ab2aa601ce93
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-10-09 19:06:19 +0530

    Time: 1 ms (58.43%), Space: 41.2 MB (90.31%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,18 +1,40 @@
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//        Stack<Character> st=new Stack<>();
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             if(ch=='(' || st.isEmpty()){
+//                 st.push(ch);
+//             }
+//             else if( st.peek()==')'){
+//                 st.push(ch);
+//             }
+//             else{
+//                 st.pop();
+//             }
+//         }
+//         return st.size();
+//     }
+// }
+
+
+
 class Solution {
     public int minAddToMakeValid(String s) {
-       Stack<Character> st=new Stack<>();
+       int open=0;
+        int close=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='(' || st.isEmpty()){
-                st.push(ch);
+            if(ch=='(' ){
+                open++;
             }
-            else if( st.peek()==')'){
-                st.push(ch);
+            else if( open<=0){
+                close++;
             }
             else{
-                st.pop();
+                open--;
             }
         }
-        return st.size();
+        return (open+close);
     }
 }
