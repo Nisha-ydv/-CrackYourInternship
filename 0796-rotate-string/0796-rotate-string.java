ce6006c5af500ac4314a7bcfbf90af90beb0f888
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-11-04 19:51:38 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (89.23%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,20 +1,30 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        String dobles=s+s;
+        return dobles.contains(goal);
+        
+    }
+}
+
 // class Solution {
 //     public boolean rotateString(String s, String goal) {
+//         // Check if the lengths are different; if so, return false
+//         if (s.length() != goal.length()) {
+//             return false;
+//         }
         
+//         // Concatenate s with itself
+//         String doubleS = s + s;
+        
+//         // Check if goal is a substring of the concatenated string
+//         return doubleS.contains(goal);
 //     }
 // }
 
-class Solution {
-    public boolean rotateString(String s, String goal) {
-        // Check if the lengths are different; if so, return false
-        if (s.length() != goal.length()) {
-            return false;
-        }
-        
-        // Concatenate s with itself
-        String doubleS = s + s;
-        
-        // Check if goal is a substring of the concatenated string
-        return doubleS.contains(goal);
-    }
-}
+
+
+
+
