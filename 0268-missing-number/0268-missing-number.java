2b269df977fc7543a8064412531645d5593299f3
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-11-20 12:31:53 +0530

    Time: 173 ms (5.07%), Space: 45.3 MB (45.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,28 @@
-class Solution {
-    public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-           if(nums[i]!=i) return i; 
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         Arrays.sort(nums);
+//         for(int i=0;i<nums.length;i++){
+//            if(nums[i]!=i) return i; 
+//         }
+//         return nums.length;
+//     }
+// }
+
+class Solution{
+    public int missingNumber(int[] nums){
+        for(int i=0;i<=nums.length;i++){
+            boolean found=false;
+            for(int j=0;j<nums.length;j++){
+              if(i==nums[j]){
+                  found =true;
+                  break;
+              } 
+                
+            }
+            if(found==false){
+                return i;
+            }
         }
-        return nums.length;
+        return -1;
     }
 }
