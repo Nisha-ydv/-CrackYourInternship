23992dc43af4e7b1ee8db708393531f1f51e2052
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-12-06 14:02:51 +0530

    Time: 46 ms (77.72%), Space: 46.3 MB (6.82%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,62 +1,51 @@
-class Solution {
-    public int maxCount(int[] banned, int n, int maxSum) {
-        int sum=0;
-        int count=0;
-        for(int i=1;i<=n;i++){
-        boolean isbanned=false;
-            for(int j=0;j<banned.length;j++){
-                if(i==banned[j]){
-                    isbanned=true;
-                    break;
-                }
-                
-            }
-        if(isbanned){
-                 continue;                     
-                }
-                if(sum+i>maxSum){
-                    break;
-                }
-                
-            sum+=i;
-            count++;
-    }
-            
-        
-        return count;
-    }
-}
 // class Solution {
 //     public int maxCount(int[] banned, int n, int maxSum) {
-//         int sum = 0; // Tracks the sum of chosen integers
-//         int count = 0; // Tracks the number of chosen integers
-
-//         for (int i = 1; i <= n; i++) {
-//             boolean isBanned = false; // Flag to check if `i` is in the banned array
-
-//             // Check if `i` is in the banned array
-//             for (int j = 0; j < banned.length; j++) {
-//                 if (i == banned[j]) {
-//                     isBanned = true; // Mark as banned
-//                     break; // Exit the inner loop early
+//         int sum=0;
+//         int count=0;
+//         for(int i=1;i<=n;i++){
+//         boolean isbanned=false;
+//             for(int j=0;j<banned.length;j++){
+//                 if(i==banned[j]){
+//                     isbanned=true;
+//                     break;
 //                 }
+                
 //             }
-
-//             // Skip the current number if it is banned
-//             if (isBanned) {
-//                 continue;
-//             }
-
-//             // Check if adding `i` exceeds maxSum
-//             if (sum + i > maxSum) {
-//                 break; // Exit the loop as no more numbers can be added
-//             }
-
-//             // Add the current number to the sum and increment count
-//             sum += i;
+//         if(isbanned){
+//                  continue;                     
+//                 }
+//                 if(sum+i>maxSum){
+//                     break;
+//                 }
+                
+//             sum+=i;
 //             count++;
-//         }
-
-//         return count; // Return the total count of chosen numbers
+//     }
+            
+        
+//         return count;
 //     }
 // }
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+       int sum=0;
+       int count=0;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<banned.length;i++){
+            set.add(banned[i]);
+        }
+        for(int i=1;i<=n;i++){
+            if(!set.contains(i)){
+             if(sum+i>maxSum){
+                 break;
+             } 
+                sum+=i;
+                count++;
+            }
+                
+            
+        }
+        return count;
+        
+}
+}
