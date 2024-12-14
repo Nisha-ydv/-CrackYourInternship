dc6a0f8a6017d810c18a0b8d31472aeb6c671a7c
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-12-14 21:53:25 +0530

    Time: 3 ms (94.44%), Space: 57.8 MB (77.00%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -1,20 +1,49 @@
+// class Solution {
+//     public long continuousSubarrays(int[] nums) {
+//         int n=nums.length;
+//         int start=0;
+//         int end=0;
+//         long count=0;
+//         TreeMap<Integer, Integer> map=new TreeMap<>();
+//         while(end<n){
+//             map.put(nums[end], map.getOrDefault(nums[end],0)+1);
+//             while((map.lastEntry().getKey()-map.firstEntry().getKey())>2){
+//                 map.put(nums[start], map.getOrDefault(nums[start],0)-1);
+//                 if(map.get(nums[start])==0){
+//                     map.remove(nums[start]);
+//                 }
+//                 start++;
+//             }
+//             count+=end-start+1;
+//             end++;
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public long continuousSubarrays(int[] nums) {
         int n=nums.length;
         int start=0;
         int end=0;
+        int currMin=Integer.MAX_VALUE;
+        int currMax=Integer.MIN_VALUE;
         long count=0;
-        TreeMap<Integer, Integer> map=new TreeMap<>();
         while(end<n){
-            map.put(nums[end], map.getOrDefault(nums[end],0)+1);
-            while((map.lastEntry().getKey()-map.firstEntry().getKey())>2){
-                map.put(nums[start], map.getOrDefault(nums[start],0)-1);
-                if(map.get(nums[start])==0){
-                    map.remove(nums[start]);
-                }
-                start++;
+            currMin=Math.min(currMin,nums[end]);
+             currMax=Math.max(currMax,nums[end]);
+            if(currMax-currMin >2){
+                start=end;
+                currMin=nums[end];
+                currMax=nums[end];
+               while(start-1>=0 && Math.abs(nums[start-1]-nums[end])<=2){
+                start--;
+                currMin=Math.min(currMin,nums[start]);
+                 currMax=Math.max(currMax,nums[start]);
+              }
             }
-            count+=end-start+1;
+           
+            count+=(end-start)+1;
             end++;
         }
         return count;
