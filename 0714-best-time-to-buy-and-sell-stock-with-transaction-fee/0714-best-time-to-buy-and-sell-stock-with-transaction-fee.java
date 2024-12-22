7275a07097ddf0b5a0866346d71f7e75c19a836d
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-12-22 19:46:26 +0530

    Time: 20 ms (40.22%), Space: 57.1 MB (28.97%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,43 @@
+// class Solution {
+//     public int f(int ind, int buy, int[] prices, int n , int fee,int[][] dp){
+//         if(ind==n) return 0;
+//         if(dp[ind][buy]!=-1) return dp[ind][buy];
+//         if(buy==1){
+//             return Math.max((-prices[ind]+f(ind+1, 0, prices, n , fee, dp)), f(ind+1, 1, prices, n , fee, dp));
+//         }
+//         else{
+//             return Math.max((prices[ind]-fee+f(ind+1, 1, prices, n , fee, dp)), f(ind+1, 0, prices, n , fee, dp));
+//         }
+//     }
+//     public int maxProfit(int[] prices, int fee) {
+//         int n=prices.length;
+//         int[][] dp=new int[n][2];
+        
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<2;j++){
+//                 dp[i][j]=-1;
+//             }
+//         }
+//         return f(0,1,prices, n, fee, dp);
+//     }
+// }
+
+class Solution {
+    
+    public int maxProfit(int[] prices, int fee) {
+        int n=prices.length;
+        int[][] dp=new int[n+1][2];
+        
+        for(int ind=n-1;ind>=0;ind--){
+           for(int buy=0;buy<=1;buy++){
+            if(buy==1){
+            dp[ind][buy]= Math.max((-prices[ind]+dp[ind+1][0]), dp[ind+1][1]);
+        }
+        else{
+            dp[ind][buy]= Math.max((prices[ind]-fee+dp[ind+1][1]),dp[ind+1][0]);
+        }   
+           } 
+        }
+        return dp[0][1];
+    }
+}
