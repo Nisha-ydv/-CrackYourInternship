f93fdbc2ee76b42932ec35a1bb23464461364c51
Nisha-ydv <156039480+Nisha-ydv@users.noreply.github.com>
2024-12-22 20:03:34 +0530

    Time: 7 ms (42.51%), Space: 45.3 MB (19.55%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -30,16 +30,14 @@ class Solution {
         int n=prices.length;
         int[][][] dp = new int[n + 1][2][k + 1];
         
-        // As dp array is initialized to 0, we have already covered the base case
         
-        // Iterate through the array and fill in the dp array
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int cap = 1; cap <= k; cap++) {
-                    if (buy == 0) { // We can buy the stock
+                    if (buy == 0) { 
                         dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][0][cap], 
                                 -prices[ind] + dp[ind + 1][1][cap]);
-                    } else { // We can sell the stock
+                    } else { 
                         dp[ind][buy][cap] = Math.max(0 + dp[ind + 1][1][cap],
                                 prices[ind] + dp[ind + 1][0][cap - 1]);
                     }
